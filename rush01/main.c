3427732c186981f589f438fa73319b7351f9365f
agasnier <gasnieralexandre@icloud.com>
2025-07-20 08:53:29 +0200

    rush 01

--- rush01/main.c
+++ rush01/main.c
@@ -1,171 +1,83 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: algasnie <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/19 14:09:03 by algasnie          #+#    #+#             */
-/*   Updated: 2025/07/19 16:50:43 by algasnie         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 
-//verificiation de l'entrée
-int	ft_entry(char *argv[])
-{
-	int i;
-
-	i = 0;
-	while (argv[1][i])
-	{
-		if (i % 2 == 0)
-		{
-			if (argv[1][i] < '1' || argv[1][i] > '4')
-				return (1);
-		}
-		else
-		{
-			if (argv[1][i] != ' ')
-				return (1);
-		}
-		i++;
-	}
-	if (i != 31)
-		return (1);
-	return (0);
-}
-
-//creation du tableau + remplissage de zero
-char	**ft_init(int len)
-{
-	char	**tab; 
-	int		i;
-	int		j;
-
-	tab = (char **)malloc(sizeof(char *) * len);
-	i = 0;
-	while (i < len)
-	{
-		tab[i] = (char *)malloc(sizeof(char) * len);
-		
-//pas obligatoire ............
-		j = 0;
-		while(j < len)
-		{
-			tab[i][j] = ' ';
-			j++;
-		}
-		i++;
-	}
-	return (tab);
-}
-
-//ajout des parametres dans le tableau
-void	ft_param(char **tab, char **argv, int len)
-{
-	int i;
-	int	j;
-
-	i = 1;
-	j = 0;
-	len -= 2;
-	while (j < (len * 2) * 4)
-	{
-		if (j < len * 2)
-			tab[0][i] = argv[1][j];
-
-		if (j >= len * 2 && j < (len * 2) * 2)
-			tab[len + 1][i - len] = argv[1][j];
-
-		if (j >= (len * 2) * 2 && j < (len * 2) * 3)
-			tab[i - len * 2][0] = argv[1][j];
-
-		if (j >= (len * 2) * 3 && j < (len * 2) * 4)
-			tab[i - len * 3][len + 1] = argv[1][j];
-		i++;
-		j += 2;
-	}
-}
+//initialisation.c
+int	ft_entry(char *argv[]);
+char **ft_init(int len);
+void	ft_param(char **tab, char **argv, int len);
 
 
 //affichage du tableau
-void	ft_putgrid(char **tab, int len)
-{
-	int i;
-	int j;
+void	ft_putgrid(char **tab, int len);
 
-	j = 0;
-	while (j < len)
-	{
-	i = 0;
-		while (i < len)
-		{
-			write(1, &tab[j][i], 1);
-			write(1, " ", 1);
-			i++;
-		}
-	write(1, "\n", 1);
-	j++;
-	}
-}
+
+
+
 
 
 int	main(int argc, char *argv[])
 {
+	//mettre len et param
+
+
+
+	//possible de trouver la taille avec le nombre d'arguement
+	//dimension du tableau len+2 pour param + 
+	int	len;
+	char **tab;
+
+	len = 4;
+	len += 2;
+
+
 	//parsing de argv pour empecher les erreurs
 	if (argc != 2)
 		return (1);
 	if (ft_entry(argv) == 1)
 		return (1);
 
-
-	//dimension du tableau + creation de celui-ci
-	int len = 4;
-	len += 2;
-	char **tab = ft_init(len);
+	//creation tab
+	tab = ft_init(len);
 	ft_param(tab, argv, len);
 
-
-
+	
+	//test test test test
 	//remplissage des solutions
 	char	sol[] = "1234";
 
-	int h = 0;
+	int	h = 0;
 	while (sol[h] != '\0')
 	{
 		tab[1][h + 1] = sol[h];
 		h++;
 	}
 
-	//comptage des solutions pour verification
-	int f = 0;
-	int ctr = 0;
-	char max = '0';
+	//comptage pour les verifications
+	int	f;
+	int	comp;
+	char	max;
+
+	f = 0;
+	comp = 0;
+	max = '0';
 
 	while (sol[f] != '\0')
 	{
 		if (sol[f] > max)
 		{
 			max = sol[f];
-			ctr++;
+			comp++;
 		}
 		f++;
 	}
 
-
-	printf("\n\n%d\n\n", ctr);
+	printf("\n\n%d\n\n", comp);
 
 
 	//affichage du tableau
-    ft_putgrid(tab, len);
-
+	ft_putgrid(tab, len);
 	printf("%s", "\n\n");
 
 
-
-	return (0);
 }
+
+
