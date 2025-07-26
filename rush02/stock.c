d2698a4c85ec286639a9c73630bba5f236fea56f
Alexandre Gasnier <algasnie@2C3.42angouleme.fr>
2025-07-26 10:59:55 +0200

    07261059

---
+++ rush02/stock.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stock.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: algasnie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/26 09:13:13 by algasnie          #+#    #+#             */
+/*   Updated: 2025/07/26 10:58:53 by algasnie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//open,read
+#include <fcntl.h>
+
+//close
+#include <unistd.h>
+
+//malloc
+#include <stdlib.h>
+
+//printf
+#include <stdio.h>
+
+char	*ft_stock(void)
+{
+//declaration variable
+	int		fd;
+	int		t_read;
+	char	tmp[1024];
+	char	*str;
+	int		i;
+	
+//ouvre le document, retourne (-1) si erreur
+	fd = open("numbers.dict", O_RDONLY);
+	if (fd == -1)
+		return (NULL);
+
+//read --> donne la taille lu en octet jusqu'a la limite (ici 1024 octets)
+//stocke ce aui est lu dans le tmp[]
+
+	////////////////////////////////////
+//Faire une boucle au cas ou buffer trop petit avec while t_read > 0
+	//////////////////////////////////
+	t_read = read(fd, tmp, 1024);
+	if (t_read == - 1)
+		return (NULL);
+	
+	tmp[t_read + 1] = '\0';
+
+
+//creer un tableau avec malloc de la taille totale du document
+	str = malloc((sizeof(char) * t_read) + 1);
+
+//remplissage du tableau avec tmp
+	i = 0;
+	while (tmp[i])
+	{
+		str[i] = tmp[i];
+		i++;
+	}
+	str[i] = '\0';
+	
+//ferme le document ouvert
+	close(fd);
+
+//retourner le tableau cree
+	return (str);
+}
+
+struct dict	**ft_struct(char *str)
+{
+// count le nombre de paires
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] == '\n')
+			count++;
+		i++;
+	}
+
+	printf("Nombre de paires: %d\n", count);
+
+	struct	dict
+	{
+		char	*number;
+		char	*carac;
+
+	};
+
+	struct dict **paires;
+	
+	i = 0;
+	while (str[i] != '\n')
+	{
+		printf("%c", str[i]);
+		i++;
+	}
+	
+	paires = malloc((sizeof(struct dict *) * count) + 1);
+	if (paires == NULL)
+		return(NULL);
+
+	return (paires);
+}
+
+int	main(void)
+{
+	char *str;
+
+	str = ft_stock();
+	//printf("%s", str);
+	ft_struct(str);
+
+
+
+	return (0);
+}
