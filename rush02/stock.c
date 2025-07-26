2208b441fdbbdbbd72122226cce391e808aa1a2b
Alexandre Gasnier <algasnie@2C3.42angouleme.fr>
2025-07-26 14:31:46 +0200

    07261431

--- rush02/stock.c
+++ rush02/stock.c
@@ -6,24 +6,16 @@
 /*   By: algasnie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 09:13:13 by algasnie          #+#    #+#             */
-/*   Updated: 2025/07/26 13:54:00 by algasnie         ###   ########.fr       */
+/*   Updated: 2025/07/26 14:24:40 by algasnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//open,read
 #include <fcntl.h>
-
-//close
 #include <unistd.h>
-
-//malloc
-#include <stdlib.h>
-
-//printf
+#include <siftdlib.h>
 #include <stdio.h>
 
-
-struct	dict
+struct	s_dict
 {
 	char	*number;
 	char	*carac;
@@ -31,35 +23,20 @@ struct	dict
 
 char	*ft_stock(void)
 {
-//declaration variable
 	int		fd;
 	int		t_read;
 	char	tmp[1024];
 	char	*str;
 	int		i;
-	
-//ouvre le document, retourne (-1) si erreur
+
 	fd = open("numbers.dict", O_RDONLY);
 	if (fd == -1)
 		return (NULL);
-
-//read --> donne la taille lu en octet jusqu'a la limite (ici 1024 octets)
-//stocke ce aui est lu dans le tmp[]
-
-	////////////////////////////////////
-//Faire une boucle au cas ou buffer trop petit avec while t_read > 0
-	//////////////////////////////////
 	t_read = read(fd, tmp, 1024);
-	if (t_read == - 1)
+	if (t_read == -1)
 		return (NULL);
-	
 	tmp[t_read] = '\0';
-
-
-//creer un str avec malloc de la taille totale du document
 	str = malloc((sizeof(char) * t_read) + 1);
-
-//remplissage du tableau avec tmp
 	i = 0;
 	while (tmp[i])
 	{
@@ -67,11 +44,7 @@ char	*ft_stock(void)
 		i++;
 	}
 	str[i] = '\0';
-	
-//ferme le document ouvert
 	close(fd);
-
-//retourner le tableau cree
 	return (str);
 }
 
@@ -86,17 +59,14 @@ void	ft_puttab(char *dst, char *src, int n)
 		i++;
 	}
 	dst[i] = '\0';
-
-	
 }
 
-struct dict **ft_pars(struct dict **paires, char *str, int count)
+struct dict	**ft_pars(struct s_dict **paires, char *str)
 {
 	int	i;
 	int	index;
 	int	j;
-	
-	count = 0;
+
 	i = 0;
 	index = 0;
 	while (i < 1)
@@ -108,7 +78,6 @@ struct dict **ft_pars(struct dict **paires, char *str, int count)
 				j++;
 			paires[index]->number = malloc(sizeof(char) * (j + 1));
 			ft_puttab(paires[index]->number, &str[i], j);
-
 		}
 		else if (str[i] >= '!' && str[i] <= '~')
 		{
@@ -116,61 +85,49 @@ struct dict **ft_pars(struct dict **paires, char *str, int count)
 				j++;
 			paires[index]->carac = malloc(sizeof(char) * (j + 1));
 			ft_puttab(paires[index]->carac, &str[i], j);
-
 		}
 		else if (str[i] == '\n' && str[i + 1] != '\0')
 			index++;
 		i++;
 	}
-
-
 	return (paires);
 }
 
-
-
 struct dict	**ft_struct(char *str)
 {
-// count le nombre de paires
-	int	count;
-	int	i;
+	int			count;
+	int			i;
+	struct dict	**paires;
 
 	count = 0;
 	i = 0;
-	while(str[i] != '\0')
-	{
+	while (str[i++] != '\0')
 		if (str[i] == '\n')
 			count++;
+	paires = malloc(sizeof(struct s_dict *) * (count + 1));
+	if (paires == NULL)
+		return (NULL);
+	i = 0;
+	while (i < count)
+	{
+		paires[i] = malloc(sizeof(struct s_dict));
+		if (paires[i] == NULL)
+			return (NULL);
 		i++;
 	}
-
-	printf("Nombre de paires: %d\n\n", count);
-
-//creer le tableau de strucs
-
-
-	struct dict **paires;
-	
-	paires = malloc(sizeof(struct dict *) * (count + 1));
-	if (paires == NULL)
-		return(NULL);
-	paires[count] = NULL;
-
-	ft_pars(paires, str, count);
-
+	paires[i] = NULL;
+	ft_pars(paires, str);
 	return (paires);
 }
 
 int	main(void)
 {
-
-	char *str;
+	struct dict	**dict;
+	char		*str;
 
 	str = ft_stock();
 	printf("\nImpression du dict:\n%s\n", str);
-	ft_struct(str);
-
-
-
+	dict = ft_struct(str);
+	printf("%s", dict[1]->number);
 	return (0);
 }
